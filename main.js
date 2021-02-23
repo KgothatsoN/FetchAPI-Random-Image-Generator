18d8580406b365408f3aa93796e9a9a73b3ca3e2
KgothatsoN <kg.otha@hotmail.com>
2021-02-23 22:23:44 +0000

    Randomise button event added with Fetch API

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
+const randomButton = document.getElementById("randomButton")
+const clearButton = document.getElementById("clearButton")
 
+randomButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    fetch("https://picsum.photos/200/300")
+    .then(res => res.blob())
+    .then(blob => {
+        let img = document.createElement("img");
+        img.src = URL.createObjectURL(blob);
+        document.querySelector(".image").appendChild(img);
+    });
+})
 
